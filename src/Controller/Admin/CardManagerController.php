84130df041d406f09a7bb56bb1f15cb0b1470f55
Ainkra <felke2101@gmail.com>
2023-01-20 09:32:50 +0100

    feat: Create controller for managing card.

---
+++ src/Controller/Admin/CardManagerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class CardManagerController extends AbstractController
+{
+    public function cardManager() : Response
+    {
+        return $this -> render('cardManager.html.twig');
+    }
+}
