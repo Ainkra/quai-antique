e7a3d8a427339fe93ac10193dfca93202a3701fa
Ainkra <felke2101@gmail.com>
2023-01-30 13:39:39 +0100

    feat: update controller

--- src/Controller/Admin/GaleryManagerController.php
+++ src/Controller/Admin/GaleryManagerController.php
@@ -4,9 +4,16 @@ namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin/galeryManager", name="admin_galeryManager")
+ */
 class GaleryManagerController extends AbstractController
 {
+    /**
+     * @Route("/", name="admin_galeryManager_index", methods={"GET"})
+     */
     public function galeryManager() : Response
     {
         return $this -> render('Admin/galeryManager.html.twig');
