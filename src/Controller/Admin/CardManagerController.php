cc131a6d074c38e609584a3deefb36c68b424ac4
Ainkra <felke2101@gmail.com>
2023-01-30 13:39:36 +0100

    feat: update controller

--- src/Controller/Admin/CardManagerController.php
+++ src/Controller/Admin/CardManagerController.php
@@ -4,9 +4,16 @@ namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin/cardManager", name="admin_cardManager")
+ */
 class CardManagerController extends AbstractController
 {
+    /**
+     * @Route("/", name="admin_cardManager_index", methods={"GET"})
+     */
     public function cardManager() : Response
     {
         return $this -> render('Admin/cardManager.html.twig');
