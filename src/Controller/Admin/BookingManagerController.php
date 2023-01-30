abc16e3f2fd59a15c52b7b59a4a8405a3e8749f6
Ainkra <felke2101@gmail.com>
2023-01-30 13:38:15 +0100

    feat: update controller

--- src/Controller/Admin/BookingManagerController.php
+++ src/Controller/Admin/BookingManagerController.php
@@ -4,9 +4,16 @@ namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin/galeryManager", name="admin_bookingManager")
+ */
 class BookingManagerController extends AbstractController
 {
+    /**
+     * @Route("/", name="admin_bookingManager_index", methods={"GET"})
+     */
     public function bookingManager() : Response
     {
         return $this -> render('Admin/bookingManager.html.twig');
