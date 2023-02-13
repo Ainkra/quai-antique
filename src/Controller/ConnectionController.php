4c0a6bba17681036faab293fc4f0b706150252e6
Ainkra <felke2101@gmail.com>
2023-02-13 09:34:10 +0100

    fix: move code in UserController.php

--- src/Controller/ConnectionController.php
+++ src/Controller/ConnectionController.php
@@ -3,35 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use App\Entity\User;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ConnectionController extends AbstractController
 {
-    // rending connection page
-    public function connection() : Response
+    #[Route('/connection', name:'connection')]
+    public function connection()
     {
         return $this -> render('connection.html.twig');
-    }
-
-    public function buildConnectionForm(FormBuilderInterface $builder) {
-        $builder
-            ->add('email', EmailType::class)
-            ->add('password', PasswordType::class)
-            ->add('submit', SubmitType::class, [
-                'label' => 'Envoyer'
-            ]);
-    }
-
-    public function configureOptions(OptionsResolver $resolver) {
-        $resolver->setDefaults([
-            'data_class' => User::class
-        ]);
     }
 }
