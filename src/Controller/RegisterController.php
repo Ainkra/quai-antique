9b0ba3e6fad2352fdda5be1adb5356dc39a4d783
Ainkra <felke2101@gmail.com>
2023-02-15 16:37:34 +0100

    feat: update RegisterController

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -9,52 +9,75 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Form\RegisterType;
 use App\Entity\Customer;
-use Doctrine\ORM\NoResultException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+// use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class RegisterController extends AbstractController
 {
-
-    public function handleFormSubmission($formData, ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher)
+    // register route
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher) : Response  
     {
-        try {
-            // create a new user entity and set its properties
-            $user = new Customer();
-            $plainTextPassword = $user->getPassword($formData['password']);
-            $hashedPassword = $passwordHasher->hashPassword($user, $plainTextPassword);
+        $user = new Customer(); 
+        $form = $this->createForm(RegisterType::class);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = $form->get('email')->getData();
+            $formData = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $existingUser = $entityManager->getRepository(Customer::class)->findBy(['email' => $email]);
+            $password = $form->get('password')->getData();
+            $confirmPassword = $form->get('confirm')->getData();
 
-            $user->setEmail($formData['email']);
+
+            if ($password !== $confirmPassword) {
+                $message = 'Les mots de passe ne sont pas identiques.';
+                $messageType = 'error';
+    
+                return $this->render('Register/register.html.twig', [
+                    'register' => $form->createView(),
+                    'message' => $message,
+                    'messageType' => $messageType
+                ]);
+            }
+    
+            if ($existingUser) {
+                $message = 'Email déjà utilisé, veuillez en utiliser un autre.';
+                $messageType = 'error';
+    
+                return $this->render('Register/register.html.twig', [
+                    'register' => $form->createView(),
+                    'message' => $message,
+                    'messageType' => $messageType
+                ]);
+            }
+
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $formData['password']
+            );
+    
             $user->setPassword($hashedPassword);
+            $user->setEmail($formData['email']);
             $user->setGuestNumber($formData['guestNumber']);
             $user->setAllergies($formData['allergies']);
-    
-            // persist the user entity to the database
-            $entityManager = $doctrine->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
-
+            $message = 'Vous avez bien été enregistré(e) !';
+            $messageType = 'success';
     
-        } catch (NoResultException $e) {
-            echo $e;
-        }
-    }
-
-    #[Route('/register', name: 'app_register')]
-    public function registerRender(Request $request, ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher): Response
-    {
-        $form = $this->createForm(RegisterType::class);
-        $message = null;
-    
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->handleFormSubmission($form->getData(), $doctrine, $passwordHasher);
-            $message = 'Le formulaire a été envoyé avec succès';
+            $form = $this->createForm(RegisterType::class);
+            return $this->render('Register/register.html.twig', [
+                'register' => $form->createView(),
+                'message' => $message,
+                'messageType' => $messageType
+            ]);
         }
     
+        // Si le formulaire n'est pas encore soumis ou est invalide, on renvoie une réponse avec le formulaire
         return $this->render('Register/register.html.twig', [
-            'register' => $form->createView(),
-            'message' => $message
+            'register' => $form->createView()
         ]);
     }
 }
