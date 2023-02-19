b29448927b171a5511166cce0f92d32b8074f320
Ainkra <felke2101@gmail.com>
2023-02-19 15:40:48 +0100

    feat: update AdminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,25 +7,52 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
-    public function index(Request $request, AuthenticationUtils $authenticationUtils) : Response
+    #[Route('/admin', name: 'admin')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function adminLogin(Request $request, AuthenticationUtils $authenticationUtils, Security $security) : Response
     {
+        if ($security->getUser()) {
+            return $this->render('Admin/admin.html.twig', [
+                'controller_name' => 'GaleryController',
+            ]);
+        }
+
         // Get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // Last email entered by the user
         $lastEmail = $authenticationUtils->getLastUsername();
 
         // Render the login form
-        return $this->render('admin/login.html.twig', [
+        return $this->render('Admin/admin.html.twig', [
             'last_email' => $lastEmail,
             'error' => $error
         ]);
+    }
 
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+    #[Route('/admin/cardManager', name: 'admin_cardManager')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function cardManager(Request $request, AuthenticationUtils $authenticationUtils, Security $security) : Response
+    {
+        if ($security->getUser()) {
+            return $this->render('Admin/admin.html.twig', [
+                'controller_name' => 'GaleryController',
+            ]);
+        }
+
+        // Get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // Last email entered by the user
+        $lastEmail = $authenticationUtils->getLastUsername();
+
+        // Render the login form
+        return $this->render('Admin/admin.html.twig', [
+            'last_email' => $lastEmail,
+            'error' => $error
         ]);
     }
 }
