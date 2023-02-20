95de70b0a79f6217479e4bcde837032f217064b9
Ainkra <felke2101@gmail.com>
2023-02-20 20:54:32 +0100

    feat: update CellarType

--- src/Form/CellarType.php
+++ src/Form/CellarType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Cellar;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,8 +13,18 @@ class CellarType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
+            ->add('description', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
         ;
     }
 
