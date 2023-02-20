3b8e667b60d6318899d2a718b854e66432c42b11
Ainkra <felke2101@gmail.com>
2023-02-20 20:54:48 +0100

    feat: update DishType

--- src/Form/DishType.php
+++ src/Form/DishType.php
@@ -6,15 +6,31 @@ use App\Entity\Dish;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class DishType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('price')
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
+            ->add('description', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
+            ->add('price', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
         ;
     }
 
