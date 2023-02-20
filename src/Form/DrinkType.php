04cdd9bbf8b697c8b62a851130ccb07d9e49b890
Ainkra <felke2101@gmail.com>
2023-02-20 20:54:58 +0100

    feat: update DrinkType

--- src/Form/DrinkType.php
+++ src/Form/DrinkType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Drink;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,8 +13,18 @@ class DrinkType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('price')
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
+            ->add('price', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
         ;
     }
 
