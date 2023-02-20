2f8443083402a5cf707db0b51c8bac59675a1198
Ainkra <felke2101@gmail.com>
2023-02-20 20:54:40 +0100

    feat: update DessertType

--- src/Form/DessertType.php
+++ src/Form/DessertType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Desserts;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,9 +13,24 @@ class DessertType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('price')
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
+            ->add('description', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
+            ->add('price', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
         ;
     }
 
