c42fbae9ec0f4bf7a34902f7480ca1c41b18222e
Ainkra <felke2101@gmail.com>
2023-02-21 00:24:48 +0100

    feat: update DessertType

--- src/Form/DessertType.php
+++ src/Form/DessertType.php
@@ -15,19 +15,22 @@ class DessertType extends AbstractType
         $builder
             ->add('title', TextType::class, [
                 'attr' => [
-                    'class' => 'card-input'
+                    'class' => 'card-input',
+                    'placeholder' => 'titre'
                 ],
                 "required" => true
             ])
             ->add('description', TextType::class, [
                 'attr' => [
-                    'class' => 'card-input'
+                    'class' => 'card-input',
+                    'placeholder' => 'description'
                 ],
                 "required" => true
             ])
             ->add('price', TextType::class, [
                 'attr' => [
-                    'class' => 'card-input'
+                    'class' => 'card-input',
+                    'placeholder' => 'prix'
                 ],
                 "required" => true
             ])
