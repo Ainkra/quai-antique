faf1092dc00dd05f269d0654dc1d1db3eb570c93
Ainkra <felke2101@gmail.com>
2023-02-20 20:54:24 +0100

    feat: update AperitifType

--- src/Form/AperitifType.php
+++ src/Form/AperitifType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Aperitif;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,9 +13,24 @@ class AperitifType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('price')
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
+            ->add('description', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
+            ->add('price', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input'
+                ],
+                "required" => true
+            ])
         ;
     }
 
