0b609431f4b0eddbb12d41b24e269a5b8ab845d0
Ainkra <felke2101@gmail.com>
2023-02-21 00:23:51 +0100

    feat: update CardController

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -2,9 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Desserts;
+use App\Entity\Starter;
+use App\Entity\Dish;
+use App\Entity\Drink;
+use App\Entity\Cellar;
+use App\Entity\Aperitif;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 
 class CardController extends AbstractController
 {
@@ -13,6 +21,32 @@ class CardController extends AbstractController
     {
         return $this->render('Card/card.html.twig', [
             'controller_name' => 'CardController',
+        ]);
+    }
+
+    public function getEntities(string $entityClass, ManagerRegistry $doctrine): array
+    {
+        $repository = $doctrine->getManager()->getRepository($entityClass);
+        return $repository->findAll();
+    }
+
+    #[Route('/', name: 'app_card')]
+    public function starter(ManagerRegistry $doctrine): Response
+    {
+        $starter = $this->getEntities(Starter::class, $doctrine);
+        $aperitif = $this->getEntities(Aperitif::class, $doctrine);
+        $cellar = $this->getEntities(Cellar::class, $doctrine);
+        $desserts = $this->getEntities(Desserts::class, $doctrine);
+        $dish = $this->getEntities(Dish::class, $doctrine);
+        $drink = $this->getEntities(Drink::class, $doctrine);
+
+        return $this->render('Card/card.html.twig', [
+            'starter' => $starter,
+            'aperitif' => $aperitif,
+            'cellar' => $cellar,
+            'desserts' => $desserts,
+            'dish' => $dish,
+            'drink' => $drink
         ]);
     }
 }
