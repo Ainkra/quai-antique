26a9ef9b975c0b535f3c3738daea0f8840f26ec5
Ainkra <felke2101@gmail.com>
2023-02-21 19:56:33 +0100

    feat: comment

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -12,39 +12,45 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BookingController extends AbstractController
 {
-    
+    // Booking route 
     #[Route('/booking', name: 'app_booking')]
     public function booking(Request $request, ManagerRegistry $doctrine): Response
     {
+        // get user
         $user = $this->getUser();
 
+        // Create form and get data in form.
         $form = $this->createForm(BookingType::class);
         $form->handleRequest($request); // Handle request        
         $bookingTime = $form['bookingTime']->getData();
 
+        
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $doctrine->getManager();
 
+            // Create new booking view
             $booking = new Booking();
             $booking->setName($form['name']->getData()); // Get name data
-            $booking->setGuestNumber($form['guestNumber']->getData());
-            $booking->setDate($form['date']->getData());
-            $booking->setBookingTime(new \DateTimeImmutable($bookingTime));
-            $booking->setAllergies($form['allergies']->getData());
+            $booking->setGuestNumber($form['guestNumber']->getData()); // Get guestNumber data
+            $booking->setDate($form['date']->getData()); // Get date data
+            $booking->setBookingTime(new \DateTimeImmutable($bookingTime)); // Get hour data
+            $booking->setAllergies($form['allergies']->getData()); // Get allergies data
 
             $entityManager->persist($booking);
-            $entityManager->flush();
+            $entityManager->flush(); // send data in data base
 
-            $message = 'Votre réservation est bien envoyée !';
-            $messageType = 'success';
+            $message = 'Votre réservation est bien envoyée !'; // notification message
+            $messageType = 'success'; // Used in template for modify css class
 
-            $form = $this->createForm(BookingType::class);
+            $form = $this->createForm(BookingType::class); // form creation
 
+             // form display
             return $this->render('Booking/booking.html.twig', [
                 'booking' => $form->createView(),
                 'message' => $message,
                 'messageType' => $messageType
             ]);
+
         } else if ($form->isSubmitted()) {
             $errors = array();
             
