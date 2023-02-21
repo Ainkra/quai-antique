ed4f8b3ba3e24a95ce73b2dfb76b954159da0dd2
Ainkra <felke2101@gmail.com>
2023-02-21 20:03:44 +0100

    feat: update CardController

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -8,11 +8,13 @@ use App\Entity\Dish;
 use App\Entity\Drink;
 use App\Entity\Cellar;
 use App\Entity\Aperitif;
+use App\Form\StarterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class CardController extends AbstractController
 {
@@ -31,8 +33,9 @@ class CardController extends AbstractController
     }
 
     #[Route('/', name: 'app_card')]
-    public function starter(ManagerRegistry $doctrine): Response
+    public function cardAdder(ManagerRegistry $doctrine): Response
     {
+         // get entities
         $starter = $this->getEntities(Starter::class, $doctrine);
         $aperitif = $this->getEntities(Aperitif::class, $doctrine);
         $cellar = $this->getEntities(Cellar::class, $doctrine);
@@ -49,4 +52,56 @@ class CardController extends AbstractController
             'drink' => $drink
         ]);
     }
+
+//###########################################################
+//                      DELETE SYSTEM
+//###########################################################
+    #[Route('/starter/delete-all', name: 'starter_delete_all')]
+    public function deleteAllStarters(EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->createQuery('DELETE FROM App\Entity\Starter')->execute();
+    
+        return $this->redirectToRoute('admin');
+    }
+
+    #[Route('/dessert/delete-all', name: 'dessert_delete_all')]
+    public function deleteAllDessert(EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->createQuery('DELETE FROM App\Entity\Desserts')->execute();
+    
+        return $this->redirectToRoute('admin');
+    }
+
+    #[Route('/drink/delete-all', name: 'drink_delete_all')]
+    public function deleteAllDrink(EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->createQuery('DELETE FROM App\Entity\Drink')->execute();
+    
+        return $this->redirectToRoute('admin');
+    }
+
+    #[Route('/dish/delete-all', name: 'dish_delete_all')]
+    public function deleteAllDish(EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->createQuery('DELETE FROM App\Entity\Dish')->execute();
+    
+        return $this->redirectToRoute('admin');
+    }
+
+    #[Route('/aperitif/delete-all', name: 'aperitif_delete_all')]
+    public function deleteAllAperitif(EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->createQuery('DELETE FROM App\Entity\Aperitif')->execute();
+    
+        return $this->redirectToRoute('admin');
+    }
+
+    #[Route('/cellar/delete-all', name: 'cellar_delete_all')]
+    public function deleteAllCellar(EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->createQuery('DELETE FROM App\Entity\Cellar')->execute();
+    
+        return $this->redirectToRoute('admin');
+    }
+
 }
