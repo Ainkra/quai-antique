b9a75262a82c5082fc893c5ff751f03e3721cd07
Loïs <58104051+Ainkra@users.noreply.github.com>
2023-03-16 11:28:38 +0100

    feat: update

--- src/Controller/DishModifierController.php
+++ src/Controller/DishModifierController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Starter;
+use App\Form\StarterType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DishModifierController extends AbstractController
 {
-    #[Route('/dish/modifier', name: 'app_dish_modifier')]
-    public function dishModifier(): Response
+    #[Route('admin/dishModifier', name: 'admin_dishModifier')]
+    public function dishModifier(Request $request, ManagerRegistry $doctrine, Starter $starter): Response
     {
+
+        $form = $this->createForm(StarterType::class, $starter);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_dishModifier');
+        }
+
         return $this->render('Admin/dishModifier.html.twig', [
-            'controller_name' => 'DishModifierController',
+            'form' => $form->createView(),
+            'starter' => $starter
         ]);
     }
 }
