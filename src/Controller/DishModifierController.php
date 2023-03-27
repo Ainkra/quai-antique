7ae2dc5b977c436500de85134b41c5ae232a774d
Loïs <58104051+Ainkra@users.noreply.github.com>
2023-03-27 09:23:26 +0200

    feat: update DishModifierController

--- src/Controller/DishModifierController.php
+++ src/Controller/DishModifierController.php
@@ -2,8 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Aperitif;
+use App\Entity\Dish;
+use App\Entity\Drink;
+use App\Entity\Desserts;
 use App\Entity\Starter;
+use App\Entity\Cellar;
 use App\Form\StarterType;
+use App\Form\DishType;
+use App\Form\AperitifType;
+use App\Form\CellarType;
+use App\Form\DrinkType;
+use App\Form\DessertType;
+use App\Repository\StarterRepository;
+use App\Repository\AperitifRepository;
+use App\Repository\DishRepository;
+use App\Repository\DrinkRepository;
+use App\Repository\CellarRepository;
+use App\Repository\DessertsRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,23 +29,92 @@ use Symfony\Component\Routing\Annotation\Route;
 class DishModifierController extends AbstractController
 {
     #[Route('admin/dishModifier', name: 'admin_dishModifier')]
-    public function dishModifier(Request $request, ManagerRegistry $doctrine, Starter $starter): Response
+    public function dishDisplayer(
+        StarterRepository $starterRepository, 
+        DishRepository $dishRepository, 
+        AperitifRepository $aperitifRepository, 
+        DrinkRepository $drinkRepository,
+        CellarRepository $cellarRepository,
+        DessertsRepository $dessertsRepository
+    ): Response
     {
-
-        $form = $this->createForm(StarterType::class, $starter);
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()) {
-
-            $entityManager = $doctrine->getManager();
-            $entityManager->flush();
-
-            return $this->redirectToRoute('admin_dishModifier');
-        }
+        $starters = $starterRepository->findAll();
+        $dishes = $dishRepository->findAll();
+        $aperitifs = $aperitifRepository->findAll();
+        $drinks = $drinkRepository->findAll();
+        $cellars = $cellarRepository->findAll();
+        $desserts = $dessertsRepository->findAll();
 
         return $this->render('Admin/dishModifier.html.twig', [
-            'form' => $form->createView(),
-            'starter' => $starter
+            "starters" => $starters,
+            "desserts" => $desserts,
+            "dishes" => $dishes,
+            "drinks" => $drinks,
+            "cellars" => $cellars,
+            "aperitifs" => $aperitifs,
         ]);
+    }
+
+    #[Route('admin/dishModifier/{type}/{id}/edit', name: 'admin_dishModifier_edit')]
+    public function dishModifier(Request $request, $type, $id, ManagerRegistry $doctrine): Response
+    {
+        $entityClass = $this->getEntityClass($type);
+        $entity = $doctrine->getRepository($entityClass)->find($id);
+    
+        $formType = $this->getFormType($type);
+        $form = $this->createForm($formType, $entity);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($entity);
+            $entityManager->flush();
+    
+            return $this->redirectToRoute('admin_dishModifier');
+        }
+    
+        return $this->render('Admin/dishEdit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    private function getEntityClass($type)
+    {
+        switch ($type) {
+            case 'starter':
+                return Starter::class;
+            case 'dish':
+                return Dish::class;
+            case 'drink':
+                return Drink::class;
+            case 'desserts':
+                return Desserts::class;
+            case 'aperitif':
+                return Aperitif::class;
+            case 'cellar':
+                return Cellar::class;
+        }
+
+        throw new \InvalidArgumentException("Type de plat non valide");
+    }
+
+    private function getFormType(string $type): string
+    {
+        switch ($type) {
+            case 'starter':
+                return StarterType::class;
+            case 'dish':
+                return DishType::class;
+            case 'drink':
+                return DrinkType::class;
+            case 'desserts':
+                return DessertType::class;
+            case 'aperitif':
+                return AperitifType::class;
+            case 'cellar':
+                return CellarType::class;
+            }
+
+            throw new \InvalidArgumentException("Type de plat non valide");
     }
 }
