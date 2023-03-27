e850cda6f9d0e7b2452d09fdcbec472f2539b0a8
Loïs <58104051+Ainkra@users.noreply.github.com>
2023-03-27 09:23:40 +0200

    feat: update

--- src/Form/StarterType.php
+++ src/Form/StarterType.php
@@ -7,15 +7,12 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class StarterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $urlGenerator = $options['url_generator'];
-        
+    {   
         $builder
             ->add('title', TextType::class, [
                 'attr' => [
