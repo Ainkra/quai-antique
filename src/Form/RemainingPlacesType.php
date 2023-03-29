c793531eefa67842496d35ac34eb8ccfb0937a14
Loïs <58104051+Ainkra@users.noreply.github.com>
2023-03-29 21:35:45 +0200

    feat: update form

---
+++ src/Form/RemainingPlacesType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\RemainingPlaces;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RemainingPlacesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('places', IntegerType::class, [ // Use NumberType input
+                'attr' => [
+                    'form_attr' => true,
+                    'class' => 'text-black lg:text-xl xs:text-lg',
+                    'min' => 0,
+                    
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => RemainingPlaces::class,
+        ]);
+    }
+}
