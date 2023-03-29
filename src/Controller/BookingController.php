3d21bfb26b5dd49e74ba233a66acf8e705170578
Loïs <58104051+Ainkra@users.noreply.github.com>
2023-03-29 21:34:21 +0200

    feat: update BookingController

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -21,14 +21,13 @@ class BookingController extends AbstractController
 
         // Create form and get data in form.
         $form = $this->createForm(BookingType::class);
-        $form->handleRequest($request); // Handle request        
+        $form->handleRequest($request); // Handle request
         $bookingTime = $form['bookingTime']->getData();
-
         
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $doctrine->getManager();
 
-            // Create new booking view
+            // Create new booking input
             $booking = new Booking();
             $booking->setName($form['name']->getData()); // Get name data
             $booking->setGuestNumber($form['guestNumber']->getData()); // Get guestNumber data
@@ -51,29 +50,16 @@ class BookingController extends AbstractController
                 'messageType' => $messageType
             ]);
 
-        } else if ($form->isSubmitted()) {
-            $errors = array();
-            
-            // Get the error messages for each field
-            foreach ($form->getErrors(true) as $error) {
-                if (!isset($errors[$error->getOrigin()->getName()])) {
-                    $errors[$error->getOrigin()->getName()] = array();
-                }
-                $errors[$error->getOrigin()->getName()][] = $error->getMessage();
-            }
-
-            $form = $this->createForm(BookingType::class, $user);
-
-            return $this->render('Booking/booking.html.twig', [
-                'booking' => $form->createView(),
-                'errors' => $errors
-            ]);
         }
-
-        
 
         return $this->render('Booking/booking.html.twig', [
             'booking' => $form->createView()
         ]);
+    }
+
+    #[Route('/booking/remainingPlaces', name: 'app_booking_remainingPlaces')]
+    public function remainingPlaces() 
+    {
+        
     }
 }
