fb9e09ef590be0c348aac1bcf78fc5364f4f977a
Loïs <58104051+Ainkra@users.noreply.github.com>
2023-03-29 21:35:17 +0200

    feat: update RemainingPlacesController

---
+++ src/Controller/RemainingPlacesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\RemainingPlaces;
+use App\Form\RemainingPlacesType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RemainingPlacesController extends AbstractController
+{
+    /**
+     * Permit to rendering and modify maximum places allowed in the restaurant.
+     */
+    #[Route('/admin/bookingManager', name: 'admin_bookingManager')]
+    public function remainingPlaces(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $remainingPlaceRepository = $doctrine->getRepository(RemainingPlaces::class);
+        $remainingPlaces = $remainingPlaceRepository->findOneBy([]);
+
+        $form = $this->createForm(RemainingPlacesType::class, $remainingPlaces);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($remainingPlaces);
+            $entityManager->flush();
+
+            $message = "Nombre de places maximum modifié avec succès !";
+            
+            return $this->render('Admin/bookingManager.html.twig', [
+                'form' => $form,
+                'message' => $message
+            ]);
+        }
+
+        return $this->render('Admin/bookingManager.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
