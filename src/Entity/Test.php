2b8ba95cb2518262cdb1324956c7f7b4317c1b01
Loïs <felke2101@gmail.com>
2023-06-03 09:10:31 +0200

    feat: add example

---
+++ src/Entity/Test.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TestRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TestRepository::class)]
+class Test
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $example = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getExample(): ?string
+    {
+        return $this->example;
+    }
+
+    public function setExample(string $example): self
+    {
+        $this->example = $example;
+
+        return $this;
+    }
+}
