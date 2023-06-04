ca4dbfca75b00b37bb7bc8a94d1940fe30f113e8
Loïs <felke2101@gmail.com>
2023-06-04 14:43:07 +0200

    feat: update

---
+++ src/Controller/Galery/GaleryMenuController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Galery;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class GaleryMenuController extends AbstractController
+{
+    #[Route('/admin/galeryManager', name: 'admin_galeryManager')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function galeryMenu(): Response
+    {
+        return $this->render('Admin/galeryManager.html.twig', [
+            'controller_name' => 'GaleryMenuController',
+        ]);
+    }
+}
