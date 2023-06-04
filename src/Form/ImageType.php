cd70399acbfeabd089e0310efcc3fd1111845463
Loïs <felke2101@gmail.com>
2023-06-04 14:43:39 +0200

    feat: create image form

---
+++ src/Form/ImageType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Image;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class ImageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('imageFilename', FileType::class, [
+                'attr' => [
+                    'class' => 'card-input text-white',
+                ],
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Le fichier doit être au format png ou jpeg.',
+                    ]),
+                ],
+                
+            ])
+            ->add('description', TextType::class, [
+                'attr' => [
+                    'class' => 'card-input',
+                    'placeholder' => 'description'
+                ],
+                "required" => true
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Image::class,
+        ]);
+    }
+}
